8224c06073d58baa225a574ac7e7b5288b8b4ec0
minuxx <minux8905@gmail.com>
2025-01-28 00:45:48 +0900

    🔧 chore(configure): eslint & prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  parser: 'typescript', // 타입스크립트 사용
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed', // 객체 속성 Quote 옵션
+  trailingComma: 'all', // 여러 줄의 쉼표로 구분된 구문에서 후행 콤마 사용 방식
+  bracketSpacing: true, // 객체 brackets 사이에 공백 여부
+  bracketSameLine: true, // JSX 마지막 props 옆 > 내릴지 여부
+  arrowParens: 'always', // 화살표 함수 괄호 사용 여부
+  endOfLine: 'lf',
+};
--- app/detail.tsx
+++ app/detail.tsx
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native';
 
 function Detail() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Detail</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Detail</Text>
+    </View>
+  );
 }
 
 export default Detail;

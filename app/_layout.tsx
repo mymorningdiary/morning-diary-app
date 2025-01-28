d55fc33f970724f1732237d973e7159d45e395c2
minuxx <minux8905@gmail.com>
2025-01-28 13:04:20 +0900

    🔧 chore(configure): 카카오 로그인 관련 패키지 설치 및 app.json 설정
    - expo-dev-client
    - expo-build-properties
    - @react-native-kakao/core
    - @react-native-kakao/user
    
    MD-50

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,16 @@ import { MDDarkTheme } from '@/constants/theme';
 import { MDLightTheme } from '@/constants/theme';
 import { ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { getKeyHashAndroid } from '@react-native-kakao/core';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    getKeyHashAndroid().then(console.log);
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? MDDarkTheme : MDLightTheme}>

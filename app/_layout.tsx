8644c256e4a4f3ceb83ceffa6ce23943d8ce7c76
minuxx <minux8905@gmail.com>
2025-01-29 17:52:49 +0900

    💄 style(splash): App Icon, Splash Image 추가 및 app.json 설정
    MD-75

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,31 @@
 import { MDDarkTheme } from '@/constants/theme';
 import { MDLightTheme } from '@/constants/theme';
 import { ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { useEffect } from 'react';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { useColorScheme } from 'react-native';
 import { initializeKakaoSDK } from '@react-native-kakao/core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsAppReady(true);
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    if (isAppReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [isAppReady]);
 
   useEffect(() => {
     initializeKakaoSDK('162f6841f726fc6ff11498b56b4030aa'); // TODO: 환경변수 설정

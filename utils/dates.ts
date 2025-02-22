b36d4495756ce5f7682da3e11425249ad9877b6c
minuxx <minux8905@gmail.com>
2025-02-22 16:41:44 +0900

    feat(main): main UI 구성 중
    MD-81

---
+++ utils/dates.ts
@@ -0,0 +1,14 @@
+export const formatCalendarDate = (date: Date = new Date()): string => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
+
+export const formatCalendarHeaderDate = (date: string) => {
+  const d = new Date(date);
+  const year = d.getFullYear().toString().slice(2); // 년도의 마지막 2자리
+  const month = d.getMonth() + 1; // 월 (0-11이므로 1을 더함)
+  return `${year}년${month}월`;
+};

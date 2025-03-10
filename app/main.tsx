5a0253c86ca32c27d80df88b4a00de5019dc59f5
minuxx <minux8905@gmail.co>
2025-03-11 00:56:18 +0900

    style(main): 캘린더 UI 스타일 구성 중
    MD-81

--- app/main.tsx
+++ app/main.tsx
@@ -1,14 +1,38 @@
 import { MDView, MDText } from '@/components';
 import { useThemeColor } from '@/hooks';
-import { Calendar } from 'react-native-calendars';
-import { Image, StyleSheet } from 'react-native';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { formatCalendarDate, formatCalendarHeaderDate } from '@/utils/dates';
 import { MDColors } from '@/types';
 import { SpeechBubble } from '@/components';
+import { Theme } from 'react-native-calendars/src/types';
+
+LocaleConfig.locales['kr'] = {
+  monthNames: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  today: '오늘',
+};
+
+LocaleConfig.defaultLocale = 'kr';
 
 export default function Main() {
   const colors = useThemeColor();
   const styles = screenStyles({ colors });
+  const today = formatCalendarDate();
 
   return (
     <MDView style={styles.container}>
@@ -16,18 +40,17 @@ export default function Main() {
       <MDView style={styles.calendarWrapper}>
         <Calendar
           style={styles.calendarContainer}
+          theme={{
+            calendarBackground: colors.background.normal,
+          }}
           // Initially visible month. Default = now
           initialDate={formatCalendarDate()} // Today Date
           // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
           minDate={undefined}
           // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-          maxDate={undefined}
+          maxDate={today}
           // Handler which gets executed on day press. Default = undefined
           onDayPress={(day) => {
-            console.log('selected day', day);
-          }}
-          // Handler which gets executed on day long press. Default = undefined
-          onDayLongPress={(day) => {
             console.log('selected day', day);
           }}
           // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
@@ -36,8 +59,60 @@ export default function Main() {
           onMonthChange={(month) => {
             console.log('month changed', month);
           }}
+          markedDates={{
+            ['2025-03-10']: {
+              selected: true,
+              marked: false,
+            },
+            ['2025-03-09']: {
+              selected: false,
+              marked: true,
+            },
+            ['2025-03-08']: {
+              selected: true,
+              marked: true,
+            },
+          }}
+          dayComponent={({ date, state, marking }) => {
+            console.log('date', date, state, marking);
+            const isToday = date.dateString === today;
+            const isSelected = marking?.selected;
+            const isDisabled = state === 'disabled';
+
+            return (
+              <TouchableOpacity onPress={() => {}}>
+                <MDView
+                  style={[
+                    styles.dayContainer,
+                    isSelected && {
+                      borderRadius: 20,
+                      borderWidth: 1,
+                      borderColor: colors.line.normal,
+                    },
+                    isToday && {
+                      backgroundColor: colors.primary.softer,
+                      borderWidth: 0,
+                    },
+                  ]}>
+                  <MDText
+                    style={[
+                      styles.dayText,
+                      isDisabled && { color: colors.text.alternative },
+                      isSelected && {
+                        color: colors.text.brand,
+                      },
+                      isToday && {
+                        color: colors.primary.normal,
+                      },
+                    ]}>
+                    {date.day}
+                  </MDText>
+                </MDView>
+              </TouchableOpacity>
+            );
+          }}
           // Hide month navigation arrows. Default = false
-          hideArrows={false}
+          hideArrows={true}
           // Replace default arrows with custom ones (direction can be 'left' or 'right')
           renderArrow={(direction) => (
             <MDView style={{ width: 20, height: 20 }}>
@@ -74,7 +149,8 @@ export default function Main() {
         />
       </MDView>
 
-      <SpeechBubble text="sdfsdfsdfsdfkjklajdfksfjksdjfkls" />
+      <SpeechBubble text="" />
+      <Image source={require('@/assets/images/img-logo.png')} />
     </MDView>
   );
 }
@@ -82,14 +158,38 @@ export default function Main() {
 const screenStyles = ({ colors }: { colors: MDColors }) =>
   StyleSheet.create({
     container: {
-      width: '100%',
       flex: 1,
     },
     calendarWrapper: {},
     calendarContainer: {
       backgroundColor: colors.background.normal,
     },
+    dayContainer: {
+      width: 32,
+      height: 32,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 20,
+    },
+    dayText: {
+      fontFamily: 'Roboto-Regular',
+      color: colors.text.brand,
+      fontSize: 13,
+      lineHeight: 18,
+    },
   });
+
+const calendarTheme = (colors: MDColors): Theme => ({
+  calendarBackground: colors.background.normal,
+  textSectionTitleColor: colors.background.normal,
+  selectedDayBackgroundColor: colors.primary.normal,
+  selectedDayTextColor: colors.text.brand,
+  todayTextColor: colors.primary.normal,
+  dayTextColor: colors.text.brand,
+  textDisabledColor: colors.text.alternative,
+  dotColor: colors.primary.normal,
+  selectedDotColor: colors.text.brand,
+});
 
 function MainAppBar() {
   const colors = useThemeColor();

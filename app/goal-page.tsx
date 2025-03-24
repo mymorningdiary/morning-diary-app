597a3c53aa4c80159f23c9a42375c05a0ee18a7b
minuxx <minux8905@gmail.co>
2025-03-24 22:51:24 +0900

    style(goal-page): 목표 페이지 등록 화면 UI 구성
    ES-91

--- app/goal-page.tsx
+++ app/goal-page.tsx
@@ -1,9 +1,92 @@
-import { MDCol, MDText } from '@/components';
+import { MDButton, MDCol, MDText, MDView } from '@/components';
+import { GoalPageListItem } from '@/components/goal-page';
+import { useThemeColor } from '@/hooks';
+import { MDColors } from '@/types';
+import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+
+const MAX_GOAL_PAGE = 3;
+const CHAR_COUNT_PER_PAGE = 527;
 
 export default function GoalPage() {
+  const colors = useThemeColor();
+  const styles = screenStyles({ colors });
+  const [selectedGoal, setSelectedGoal] = useState(0);
+
   return (
-    <MDCol>
-      <MDText>골페이지</MDText>
+    <MDCol style={styles.container}>
+      <MDView style={styles.contentContainer}>
+        <MDText type="titleSemiBold" style={styles.title}>
+          아침일기
+          <MDText type="titleSemiBold" style={{ color: colors.primary.normal }}>
+            {' 목표'}
+          </MDText>
+          를 정해볼까요?
+        </MDText>
+        <MDText type="labelRegular" style={styles.subtitle}>
+          {
+            ' 글을 많이 쓸수록 깊은 생각을 끄집어 낼 수 있어요.\n목표는 언제든지 설정에서 바꿀 수 있어요.'
+          }
+        </MDText>
+
+        <MDCol style={styles.goalPageListContainer}>
+          {Array.from({ length: MAX_GOAL_PAGE }, (_, index) => (
+            <GoalPageListItem
+              key={index + 1}
+              goal={index + 1}
+              isSelected={selectedGoal === index + 1}
+              onContainerPress={() => setSelectedGoal(index + 1)}
+            />
+          ))}
+        </MDCol>
+      </MDView>
+
+      <MDView style={styles.bottomContainer}>
+        <MDButton
+          style={{
+            backgroundColor: selectedGoal !== 0 ? colors.primary.normal : colors.fill.alternative,
+          }}
+          textStyle={{ color: selectedGoal !== 0 ? colors.text.inversion : colors.text.normal }}
+          title={'일기쓰러 가기'}
+          disabled={selectedGoal === 0}
+          onPress={() => {}}
+        />
+      </MDView>
     </MDCol>
   );
 }
+
+const screenStyles = ({ colors }: { colors: MDColors }) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background.normal,
+    },
+    contentContainer: {
+      flex: 1,
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingHorizontal: 16,
+    },
+    title: {
+      color: colors.text.brand,
+      textAlign: 'center',
+    },
+    subtitle: {
+      color: colors.text.alternative,
+      textAlign: 'center',
+      marginTop: 4,
+    },
+    goalPageListContainer: {
+      width: '100%',
+      gap: 16,
+      marginTop: 24,
+    },
+    bottomContainer: {
+      width: '100%',
+      paddingHorizontal: 16,
+      paddingBottom: 60,
+    },
+    bottomButton: {},
+  });

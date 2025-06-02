3c1fd0fbe3880f8272c12f3227c4e84c64601729
minuxx <minux8905@gmail.co>
2025-06-02 22:54:57 +0900

    ♻️ refactor(write): 일기 작성 화면 코드 정리
    ES-68

--- app/write.tsx
+++ app/write.tsx
@@ -1,11 +1,11 @@
-import { MDCol, MDRow, MDText, MDView } from '@/components';
+import { MDRow, MDText, MDView } from '@/components';
 import { WriteAppBar, WriteProgressBar } from '@/components/write';
 import { useUser } from '@/contexts/UserContext';
 import { useThemeColor } from '@/hooks';
 import { MDColors } from '@/types';
 import { router, useLocalSearchParams } from 'expo-router';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { ScrollView, StyleSheet, TextInput, View } from 'react-native';
+import { useMemo, useState } from 'react';
+import { StyleSheet } from 'react-native';
 
 const WORD_CNT_PER_PAGE = 50;
 
@@ -28,47 +28,14 @@ export default function Write() {
     return goalPage * WORD_CNT_PER_PAGE;
   }, [user?.goalPage]);
 
-  const [pages, setPages] = useState<string[]>(['']);
-  const [currentPageIndex, setCurrentPageIndex] = useState(0);
-  const inputRefs = useRef<(TextInput | null)[]>([]);
-  const totalText = useMemo(() => pages.join(''), [pages]);
+  const [text, setText] = useState<string>('');
 
   const progress = useMemo(() => {
-    const currentWordCnt = totalText.length;
+    const currentWordCnt = text.length;
     const result = Math.floor((Math.min(currentWordCnt, targetWordCnt) / targetWordCnt) * 100);
 
-    console.log(
-      `currentWordCnt: ${currentWordCnt}, targetWordCnt: ${targetWordCnt}, result: ${result}`,
-    );
-
     return result;
-  }, [totalText, targetWordCnt]);
-
-  const scrollViewRef = useRef<ScrollView>(null);
-
-  const handleTextChange = (text: string, pageIndex: number) => {
-    const newPages = [...pages];
-    newPages[pageIndex] = text;
-
-    if (text.length >= WORD_CNT_PER_PAGE && pageIndex === pages.length - 1) {
-      setPages([...newPages, '']);
-      setTimeout(() => {
-        scrollViewRef.current?.scrollTo({
-          y: (pageIndex + 1) * (styles.page.minHeight as number),
-          animated: true,
-        });
-        setCurrentPageIndex(pageIndex + 1);
-      }, 50);
-    } else {
-      setPages(newPages);
-    }
-  };
-
-  useEffect(() => {
-    if (inputRefs.current[currentPageIndex]) {
-      inputRefs.current[currentPageIndex]?.focus();
-    }
-  }, [currentPageIndex]);
+  }, [text, targetWordCnt]);
 
   return (
     <MDView style={styles.container}>
@@ -87,43 +54,6 @@ export default function Write() {
           type="caption2Regular"
           style={styles.textGoalPage}>{`${user?.goalPage ?? 0}P`}</MDText>
       </MDRow>
-
-      <ScrollView
-        ref={scrollViewRef}
-        overScrollMode="never"
-        showsVerticalScrollIndicator={false}
-        onMomentumScrollEnd={(event) => {
-          const offsetY = event.nativeEvent.contentOffset.y;
-          const pageHeight = styles.page.minHeight as number;
-          const newPageIndex = Math.round(offsetY / pageHeight);
-
-          if (newPageIndex !== currentPageIndex) {
-            setCurrentPageIndex(newPageIndex);
-          }
-        }}>
-        {pages.map((text, index) => (
-          <MDView key={`page-${index}`}>
-            <MDCol style={styles.page}>
-              <TextInput
-                style={styles.textInput}
-                ref={(ref) => {
-                  inputRefs.current[index] = ref;
-                }}
-                value={text}
-                placeholder={index === 0 ? '오늘 아침에는 어떤 생각이 떠오르시나요?' : ''}
-                maxLength={WORD_CNT_PER_PAGE}
-                onChangeText={(value) => handleTextChange(value, index)}
-                multiline={true}
-              />
-              <MDText style={styles.pageNumber} type="labelRegular">
-                {`${index + 1}페이지`}
-              </MDText>
-            </MDCol>
-
-            {index !== pages.length - 1 && <MDView style={styles.pageDivider} />}
-          </MDView>
-        ))}
-      </ScrollView>
     </MDView>
   );
 }

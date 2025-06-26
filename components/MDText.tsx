be07c323d8d51618c433757fa2b3c3659c3bce5e
minuxx <minux8905@gmail.co>
2025-06-26 23:50:11 +0900

    💄 style(first-write): +Page1 스타일
    MD-137

--- components/MDText.tsx
+++ components/MDText.tsx
@@ -6,19 +6,23 @@ import { StyleProp, Text, TextProps, TextStyle } from 'react-native';
 export type ThemedTextProps = TextProps & {
   type?: Typography;
   style?: StyleProp<TextStyle> | undefined;
+  color?: string;
+  align?: TextStyle['textAlign'];
 };
 
 export function MDText({
   style,
   type = 'bodyRegular',
   suppressHighlighting = true,
+  color,
+  align,
   ...rest
 }: ThemedTextProps) {
   const { text } = useThemeColor();
 
   return (
     <Text
-      style={[{ color: text.normal }, FontStyles[type], style]}
+      style={[{ color: color ?? text.normal, textAlign: align }, FontStyles[type], style]}
       suppressHighlighting={suppressHighlighting}
       {...rest}
     />

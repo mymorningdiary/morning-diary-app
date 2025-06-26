d8456cb4ed9bd7422b552b8bba4f613b0c32a6f9
minuxx <minux8905@gmail.co>
2025-06-26 22:56:00 +0900

    ✨feat(first-write): +AppBar 컴포넌트 및 화면 닫기 기능
    MD-137

--- app/first-write.tsx
+++ app/first-write.tsx
@@ -1,11 +1,18 @@
 import { MDText } from '@/components';
+import AppBar from '@/domain/first-write/AppBar';
+import { router } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 
 export default function FirstWrite() {
   const styles = ScreenStyles;
 
+  const onCloseButtonPress = () => {
+    router.replace('/main');
+  };
+
   return (
     <View style={styles.container}>
+      <AppBar onCloseButtonPress={onCloseButtonPress} />
       <MDText>첫 일기 작성</MDText>
     </View>
   );

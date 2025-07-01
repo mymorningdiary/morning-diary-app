14b7095d5bd72683de4b9e48df978b7965596391
minuxx <minux8905@gmail.com>
2025-07-01 20:38:17 +0900

    ✨ feat(setting): +SettingAppBar 컴포넌트
    MD-93

--- app/setting.tsx
+++ app/setting.tsx
@@ -1,10 +1,29 @@
-import { MDText } from '@/components/MDText';
-import { View } from 'react-native';
+import SettingAppBar from '@/domain/setting/AppBar';
+import { useThemeColor } from '@/hooks';
+import { MDColors } from '@/types';
+import { router } from 'expo-router';
+import { useCallback } from 'react';
+import { View, StyleSheet } from 'react-native';
 
 export default function Setting() {
+  const colors = useThemeColor();
+  const styles = screenStyles({ colors });
+
+  const navigateBack = useCallback(() => {
+    router.back();
+  }, []);
+
   return (
-    <View>
-      <MDText>Setting</MDText>
+    <View style={styles.container}>
+      <SettingAppBar navigateBack={navigateBack} />
     </View>
   );
 }
+
+const screenStyles = ({ colors }: { colors: MDColors }) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background.normal,
+    },
+  });

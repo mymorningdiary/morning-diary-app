cf951f1b36af1a0fd7b446c0a7d131c66250754e
minuxx <minux8905@gmail.com>
2025-07-01 20:30:25 +0900

    ✨ feat(setting): +설정 화면 초기화
    MD-93

---
+++ app/setting.tsx
@@ -0,0 +1,10 @@
+import { MDText } from '@/components/MDText';
+import { View } from 'react-native';
+
+export default function Setting() {
+  return (
+    <View>
+      <MDText>Setting</MDText>
+    </View>
+  );
+}

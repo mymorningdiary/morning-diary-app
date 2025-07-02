fadab33bd0cd8d9600391806286ae97e350b63b8
minuxx <minux8905@gmail.com>
2025-07-02 23:14:32 +0900

    ✨ feat(setting): +SettingSectionListItem 컴포넌트
    MD-93

--- app/setting.tsx
+++ app/setting.tsx
@@ -1,16 +1,19 @@
-import { MDCol, MDPressable, MDRow, MDText } from '@/components';
+import { MDText } from '@/components';
 import SettingAppBar from '@/domain/setting/SettingAppBar';
 import SettingSection from '@/domain/setting/SettingSection';
+import SettingSectionListItem from '@/domain/setting/SettingSectionListItem';
 import { useThemeColor } from '@/hooks';
 import { MDColors } from '@/types';
 import { Image } from 'expo-image';
 import { router } from 'expo-router';
-import { useCallback } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { useCallback, useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 export default function Setting() {
   const colors = useThemeColor();
   const styles = screenStyles({ colors });
+
+  const [isAlarmOn, setIsAlarmOn] = useState(false);
 
   const navigateBack = useCallback(() => {
     router.back();
@@ -23,80 +26,63 @@ export default function Setting() {
       <ScrollView contentContainerStyle={styles.containerContent} overScrollMode="never">
         <View>
           <SettingSection title="계정">
-            <MDPressable style={styles.containerSectionItem}>
-              <MDText type="bodyRegular">계정 관리</MDText>
-              <MDPressable style={styles.buttonIconRight}>
+            <SettingSectionListItem
+              label="계정 관리"
+              tailComponent={
                 <Image
-                  source={require('@/assets/images/ic-chevron_right.png')}
                   style={styles.icon}
+                  source={require('@/assets/images/ic-chevron_right.png')}
                 />
-              </MDPressable>
-            </MDPressable>
+              }
+            />
           </SettingSection>
 
           <View style={styles.divider} />
 
           <SettingSection title="시스템 설정">
-            <MDRow style={styles.containerSectionItem}>
-              <MDText type="bodyRegular">알림</MDText>
-              <MDPressable style={styles.buttonIconRight}>
+            <SettingSectionListItem label="알림" />
+            <SettingSectionListItem
+              label="알림 시간"
+              disabled={!isAlarmOn}
+              tailComponent={
                 <Image
-                  source={require('@/assets/images/ic-chevron_right.png')}
                   style={styles.icon}
+                  source={require('@/assets/images/ic-chevron_right.png')}
+                  tintColor={isAlarmOn ? colors.icon.normal : colors.icon.alternative}
                 />
-              </MDPressable>
-            </MDRow>
-
-            <MDPressable style={styles.containerSectionItem}>
-              <MDText style={styles.inactiveText} type="bodyRegular">
-                알림 시간
-              </MDText>
-              <MDPressable style={styles.buttonIconRight}>
+              }
+            />
+            <SettingSectionListItem
+              label="아침일기 목표"
+              tailComponent={
                 <Image
-                  source={require('@/assets/images/ic-chevron_right.png')}
                   style={styles.icon}
-                  tintColor={colors.icon.alternative}
-                />
-              </MDPressable>
-            </MDPressable>
-
-            <MDPressable style={styles.containerSectionItem}>
-              <MDText type="bodyRegular">아침일기 목표</MDText>
-              <MDPressable style={styles.buttonIconRight}>
-                <Image
                   source={require('@/assets/images/ic-chevron_right.png')}
-                  style={styles.icon}
                 />
-              </MDPressable>
-            </MDPressable>
+              }
+            />
           </SettingSection>
 
           <View style={styles.divider} />
 
           <SettingSection title="소통">
-            <MDPressable style={styles.containerSectionItem}>
-              <MDText type="bodyRegular">의견 보내기</MDText>
-            </MDPressable>
-
-            <MDPressable style={styles.containerSectionItem}>
-              <MDText type="bodyRegular">리뷰 남기기</MDText>
-            </MDPressable>
+            <SettingSectionListItem label="의견 보내기" />
+            <SettingSectionListItem label="리뷰 남기기" />
           </SettingSection>
 
           <View style={styles.divider} />
 
           <SettingSection title="정보">
-            <MDPressable style={styles.containerSectionItem}>
-              <MDText type="bodyRegular">이용 약관</MDText>
-            </MDPressable>
-
-            <MDPressable style={styles.containerSectionItem}>
-              <MDText type="bodyRegular">개인정보처리방침</MDText>
-            </MDPressable>
-
-            <MDRow style={styles.containerSectionItem}>
-              <MDText type="bodyRegular">버전 정보 1.0.0</MDText>
-            </MDRow>
+            <SettingSectionListItem label="이용약관" />
+            <SettingSectionListItem label="개인정보처리방침" />
+            <SettingSectionListItem
+              label="버전 정보 1.0.0"
+              tailComponent={
+                <MDText type="bodyRegular" color={colors.text.alternative}>
+                  최신 버전
+                </MDText>
+              }
+            />
           </SettingSection>
         </View>
       </ScrollView>
@@ -113,30 +99,13 @@ const screenStyles = ({ colors }: { colors: MDColors }) =>
     containerContent: {
       paddingBottom: 40,
     },
-
-    containerSectionItem: {
-      height: 48,
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      paddingStart: 16,
-    },
-    buttonIconRight: {
-      height: '100%',
-      paddingHorizontal: 16,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    icon: {
-      width: 24,
-      height: 24,
-    },
     divider: {
       height: 1,
       backgroundColor: colors.line.normal,
       marginHorizontal: 16,
     },
-    inactiveText: {
-      color: colors.text.alternative,
+    icon: {
+      width: 24,
+      height: 24,
     },
   });

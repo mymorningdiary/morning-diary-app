012e137d19c9631414e0c5e2e6b36e1ccdc0b450
minuxx <minux8905@gmail.com>
2025-07-07 19:45:22 +0900

    ✨ feat(goal): +navigation back
    MD-147

--- app/goal.tsx
+++ app/goal.tsx
@@ -1,13 +1,23 @@
+import GoalAppBar from '@/domain/goal/GoalAppBar';
 import { useThemeColor } from '@/hooks';
 import { MDColors } from '@/types';
-import { useMemo } from 'react';
+import { router } from 'expo-router';
+import { useCallback, useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 export default function Goal() {
   const colors = useThemeColor();
   const styles = useMemo(() => ScreenStyles({ colors }), [colors]);
 
-  return <View style={styles.container}></View>;
+  const navigateBack = useCallback(() => {
+    router.back();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <GoalAppBar navigateBack={navigateBack} />
+    </View>
+  );
 }
 
 const ScreenStyles = ({ colors }: { colors: MDColors }) =>
---
+++ domain/goal/GoalAppBar.tsx
@@ -0,0 +1,53 @@
+import { useThemeColor } from '@/hooks';
+import { MDColors } from '@/types';
+import { Image } from 'expo-image';
+import { StyleSheet } from 'react-native';
+
+import { MDPressable, MDRow, MDText } from '@/components';
+import { useMemo } from 'react';
+
+interface Props {
+  navigateBack?: () => void;
+}
+
+export default function GoalAppBar({ navigateBack }: Props) {
+  const colors = useThemeColor();
+  const styles = useMemo(() => appBarStyles({ colors }), [colors]);
+
+  return (
+    <MDRow style={styles.container}>
+      <MDText type="titleSemiBold" style={styles.title}>
+        아침일기 목표
+      </MDText>
+      <MDPressable style={styles.buttonBack} onPress={navigateBack}>
+        <Image source={require('@/assets/images/ic-chevron_left.png')} style={styles.icon} />
+      </MDPressable>
+    </MDRow>
+  );
+}
+
+const appBarStyles = ({ colors }: { colors: MDColors }) =>
+  StyleSheet.create({
+    container: {
+      height: 48,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: colors.background.normal,
+    },
+    buttonBack: {
+      height: '100%',
+      paddingHorizontal: 16,
+      justifyContent: 'center',
+    },
+    icon: {
+      width: 24,
+      height: 24,
+    },
+    title: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      textAlign: 'center',
+      color: colors.text.brand,
+    },
+  });

a72b1a78d741a412961f702ce30f6e144567ee43
minuxx <minux8905@gmail.com>
2025-07-15 20:19:53 +0900

    ✨ feat(read-diary): +일기 조회 API fetcher 및 패치 훅
    MD-154

--- core/api/diary/apis.ts
+++ core/api/diary/apis.ts
@@ -1,7 +1,12 @@
-import { Diaries } from '@/core/types';
+import { Diaries, Diary } from '@/core/types';
 import axiosInstance from '../axios';
 import { ApiResponse } from '../types';
-import { GetDiariesRequest, PostDiariesRequest, PostDiariesResponse } from './types';
+import {
+  GetDiariesRequest,
+  PostDiariesRequest,
+  PostDiariesResponse,
+  GetDiaryRequest,
+} from './types';
 
 const diaryAPI = {
   getDiaries: async (params: GetDiariesRequest): Promise<ApiResponse<Diaries>> => {
@@ -10,6 +15,10 @@ const diaryAPI = {
   },
   postDiaries: async (body: PostDiariesRequest): Promise<ApiResponse<PostDiariesResponse>> => {
     const response = await axiosInstance.post('/diaries', body);
+    return response.data;
+  },
+  getDiary: async (params: GetDiaryRequest): Promise<ApiResponse<Diary>> => {
+    const response = await axiosInstance.get('/diaries', { params });
     return response.data;
   },
 };

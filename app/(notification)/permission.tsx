c47061884be4f6cf4537bec881763cde6fce80c5
minuxx <minux8905@gmail.co>
2025-08-05 23:03:32 +0900

    💄 style(notification): +NotificationPermissionScreen 화면 UI
    MD-94, MD-171

--- app/(notification)/permission.tsx
+++ app/(notification)/permission.tsx
@@ -1,6 +1,8 @@
-import { MDText } from '@/components';
+import { MDButton, MDLargeSpeechBubble, MDPressable, MDRow, MDText } from '@/components';
 import { useThemeColor } from '@/hooks';
 import { MDColors } from '@/types';
+import { Image } from 'expo-image';
+import { router } from 'expo-router';
 import { useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
 
@@ -8,9 +10,42 @@ export default function NotificationPermissionScreen() {
   const colors = useThemeColor();
   const styles = useMemo(() => ScreenStyles({ colors }), [colors]);
 
+  const onSkipButtonPress = () => {
+    router.back();
+  };
+
+  const onNextButtonPress = () => {
+    router.push({
+      pathname: '/(notification)',
+      params: { prevRoute: 'onboarding' },
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <MDText>Notification Permission</MDText>
+      <MDRow style={styles.containerSkipAppBar}>
+        <MDPressable style={styles.buttonSkip} onPress={onSkipButtonPress}>
+          <MDText type="labelRegular" color={colors.text.alternative}>
+            건너띄기
+          </MDText>
+        </MDPressable>
+      </MDRow>
+
+      <View style={styles.containerContent}>
+        <View style={styles.containerSun}>
+          <MDLargeSpeechBubble text="햇님이가 알림을 보내드릴게요!" />
+          <Image source={require('@/assets/images/img-sun-basic.png')} style={styles.imageSun} />
+        </View>
+        <View style={styles.containerTitle}>
+          <MDText type="titleSemiBold">알림을 허용해주세요</MDText>
+          <MDText type="labelRegular" color={colors.text.alternative} align="center">
+            {`꾸준히 아침일기를 쓸 수 있도록\n알림을 보내드릴게요`}
+          </MDText>
+        </View>
+      </View>
+      <View style={styles.containerFooter}>
+        <MDButton title="다음" onPress={onNextButtonPress} />
+      </View>
     </View>
   );
 }
@@ -18,6 +53,41 @@ export default function NotificationPermissionScreen() {
 const ScreenStyles = ({ colors }: { colors: MDColors }) =>
   StyleSheet.create({
     container: {
+      flex: 1,
       backgroundColor: colors.background.normal,
+    },
+    containerSkipAppBar: {
+      height: 48,
+      justifyContent: 'flex-end',
+    },
+    buttonSkip: {
+      height: '100%',
+      paddingHorizontal: 16,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    containerContent: {
+      flex: 1,
+      paddingHorizontal: 16,
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 24,
+    },
+    containerSun: {
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    imageSun: {
+      width: 93,
+      height: 93,
+    },
+    containerTitle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 4,
+    },
+    containerFooter: {
+      paddingHorizontal: 16,
+      paddingBottom: 60,
     },
   });

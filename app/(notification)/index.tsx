1c69e40a34e653f8db635503a67f07cba0d1dcc0
minuxx <minux8905@gmail.com>
2025-08-05 18:32:17 +0900

    🚧 wip(notification): +화면 UI 작업 중
    MD-94, MD-171

--- app/(notification)/index.tsx
+++ app/(notification)/index.tsx
@@ -1,10 +1,24 @@
-import { MDText } from '@/components';
-import { View } from 'react-native';
+import NotificationAppBar from '@/domain/notification/NotificationAppBar';
+import { useThemeColor } from '@/hooks';
+import { MDColors } from '@/types';
+import { useMemo } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 export default function NotificationScreen() {
+  const colors = useThemeColor();
+  const styles = useMemo(() => ScreenStyles({ colors }), [colors]);
+
   return (
-    <View>
-      <MDText>Notification</MDText>
+    <View style={styles.container}>
+      <NotificationAppBar option="건너띄기" />
     </View>
   );
 }
+
+const ScreenStyles = ({ colors }: { colors: MDColors }) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background.normal,
+    },
+  });

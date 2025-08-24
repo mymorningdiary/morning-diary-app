1c69a98d847ceda26e890599477dd04dd176309f
minuxx <minux8905@gmail.co>
2025-08-24 19:21:44 +0900

    ✨ feat(notification): +이전 경로에 따라 앱바 UI 조건부 렌더링
    MD-94

--- app/(notification)/index.tsx
+++ app/(notification)/index.tsx
@@ -1,6 +1,8 @@
+import { MDPressable, MDRow, MDText } from '@/components';
 import NotificationAppBar from '@/domain/notification/NotificationAppBar';
 import { useThemeColor } from '@/hooks';
 import { MDColors } from '@/types';
+import { router, useLocalSearchParams } from 'expo-router';
 import { useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
 
@@ -8,9 +10,30 @@ export default function NotificationScreen() {
   const colors = useThemeColor();
   const styles = useMemo(() => ScreenStyles({ colors }), [colors]);
 
+  const { prevRoute } = useLocalSearchParams<{ prevRoute: string }>();
+
+  const navigateBack = () => {
+    router.back();
+  };
+
+  const onSkipButtonPress = () => {
+    router.back();
+  };
+
   return (
     <View style={styles.container}>
-      <NotificationAppBar option="건너띄기" />
+      {prevRoute === 'onboarding' && (
+        <MDRow style={styles.containerSkipAppBar}>
+          <MDPressable style={styles.buttonSkip} onPress={onSkipButtonPress}>
+            <MDText type="labelRegular" color={colors.text.alternative}>
+              건너띄기
+            </MDText>
+          </MDPressable>
+        </MDRow>
+      )}
+      {prevRoute === 'setting' && (
+        <NotificationAppBar title="알림 시간" navigateBack={navigateBack} />
+      )}
     </View>
   );
 }
@@ -20,5 +43,15 @@ const ScreenStyles = ({ colors }: { colors: MDColors }) =>
     container: {
       flex: 1,
       backgroundColor: colors.background.normal,
+    },
+    containerSkipAppBar: {
+      height: 48,
+      justifyContent: 'flex-end',
+    },
+    buttonSkip: {
+      height: '100%',
+      paddingHorizontal: 16,
+      alignItems: 'center',
+      justifyContent: 'center',
     },
   });

81e4b58f60ff4d28a0084e4f28dff59fc90d74e5
minuxx <minux8905@gmail.com>
2025-08-29 22:23:33 +0900

    ✨ feat(settings): +알림 권한 허용 모달 및 기기 설정 앱 열기
    MD-174

---
+++ components/Modal/MDDefaultModal.tsx
@@ -0,0 +1,84 @@
+import { MDPressable, MDRow, MDText } from '@/components';
+import { useThemeColor } from '@/hooks';
+import { MDColors } from '@/types';
+import { useMemo } from 'react';
+import { Dimensions, Modal, StyleSheet, View } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+interface Props {
+  visible: boolean;
+  title: string;
+  negativeButton?: {
+    text: string;
+    onPress: () => void;
+  };
+  positiveButton?: {
+    text: string;
+    onPress: () => void;
+  };
+}
+
+export default function MDDefaultModal({ visible, title, negativeButton, positiveButton }: Props) {
+  const colors = useThemeColor();
+  const styles = useMemo(() => ModalStyles({ colors }), [colors]);
+
+  return (
+    <Modal animationType="fade" visible={visible} transparent>
+      <View style={styles.overlay}>
+        <View style={styles.containerContent}>
+          <MDText type="bodyRegular" style={styles.title}>
+            {title}
+          </MDText>
+
+          <MDRow style={styles.containerButton}>
+            {negativeButton && (
+              <MDPressable style={styles.button} onPress={negativeButton.onPress}>
+                <MDText style={styles.negativeButtonText}>{negativeButton.text}</MDText>
+              </MDPressable>
+            )}
+            {positiveButton && (
+              <MDPressable style={styles.button} onPress={positiveButton.onPress}>
+                <MDText style={styles.positiveButtonText}>{positiveButton.text}</MDText>
+              </MDPressable>
+            )}
+          </MDRow>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const ModalStyles = ({ colors }: { colors: MDColors }) =>
+  StyleSheet.create({
+    overlay: {
+      flex: 1,
+      backgroundColor: colors.fill.dim,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 24,
+    },
+    containerContent: {
+      width: SCREEN_WIDTH * 0.8,
+      backgroundColor: colors.background.normal,
+      borderRadius: 24,
+      gap: 12,
+      padding: 24,
+    },
+    title: {
+      color: colors.text.normal,
+    },
+    containerButton: {
+      justifyContent: 'flex-end',
+      gap: 2,
+    },
+    button: {
+      padding: 12,
+    },
+    negativeButtonText: {
+      color: colors.text.alternative,
+    },
+    positiveButtonText: {
+      color: colors.primary.normal,
+    },
+  });

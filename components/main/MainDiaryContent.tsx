708307ebf5a417e52a0908e3e8b96f76d8ac2d17
minuxx <minux8905@gmail.com>
2025-10-14 20:53:17 +0900

    ✨ feat(write-diary): +TextInput placeholder 랜덤 텍스트 노출
    MD-234

--- components/main/MainDiaryContent.tsx
+++ components/main/MainDiaryContent.tsx
@@ -11,28 +11,12 @@ import { Diary } from '@/core/types';
 import { useCallback, useState } from 'react';
 import { useFocusEffect } from 'expo-router';
 import { getRandomMessage } from '@/utils/arrays';
+import { GREETING_MESSAGES } from '@/constants/messages';
 
 type MainDiaryContentProps = {
   diaryInfo: Nullable<Diary>;
   onDiaryItemPress?: (diary: Diary) => void;
 };
-
-const GREETING_MESSAGES = [
-  '좋은 아침이에요',
-  '오늘 하루가 시작됐어요!',
-  '오늘 하루도 화이팅🧡',
-  '행복한 하루가 되길 바랄게요',
-  '평안한 하루가 되길 바랄게요',
-  '햇님이와 하루를 시작해볼까요?',
-  '오늘도 햇님이와 함께해요',
-  '햇님이랑 어제보다 더 멋진 오늘을 만들어봐요',
-  '아침의 설렘을 기록해봐요',
-  '오늘도 나를 위한 기록을 남겨볼까요?',
-  '오늘은 어떤 하루가 될까요?',
-  '좋은 아침~ 나의 기분을 말해봐요',
-  '하루의 시작을 열어볼까요?',
-  '아침에 흘러가는 감정들을 적어볼까요?',
-];
 
 export default function MainDiaryContent({ diaryInfo, onDiaryItemPress }: MainDiaryContentProps) {
   const colors = useThemeColor();

a289ded3ed4b16cedae2064dcbf6a3412e0ee60b
minuxx <minux8905@gmail.com>
2025-12-30 22:07:40 +0900

    🔧 chore(config): version 1.0.1 -> 1.0.2

--- app.config.ts
+++ app.config.ts
@@ -31,7 +31,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: 'morning-diary',
-  version: '1.0.1',
+  version: '1.0.2',
   orientation: 'portrait',
   icon: './assets/images/ic-app.png',
   scheme: 'morningdiary',

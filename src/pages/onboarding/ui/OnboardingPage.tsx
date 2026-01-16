95f7829a0fccaaec2bbb63613d07d08c044d8854
minuxx <minux8905@gmail.com>
2026-01-17 01:06:17 +0900

    ✨ feat(pages/onboarding): 다음 버튼 클릭 이벤트 로직 추가

--- src/pages/onboarding/ui/OnboardingPage.tsx
+++ src/pages/onboarding/ui/OnboardingPage.tsx
@@ -1,20 +1,45 @@
 import { MDPage } from '@shared/ui/MDPage';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { OnboardingSlideIndicator } from './OnboardingSlideIndicator';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { OnboardingSlider } from './OnboardingSlider';
+import { MDButton } from '@shared/ui/MDButton';
+import PagerView from 'react-native-pager-view';
+import { useVisit } from '@features/onboarding';
+import { router } from 'expo-router';
 
 export const SLIDE_CNT = 2;
 
 export function OnboardingPage() {
   const styles = PageStyles;
+  const sliderRef = useRef<PagerView | null>(null);
 
+  const { markVisited } = useVisit();
   const [currentPosition, setCurrentPosition] = useState(0);
+
+  const handleNextButtonPress = () => {
+    if (currentPosition < SLIDE_CNT - 1) {
+      const next = currentPosition + 1;
+
+      setCurrentPosition(next);
+      sliderRef.current?.setPage(next);
+    } else {
+      markVisited();
+      router.replace('/login');
+    }
+  };
 
   return (
     <MDPage style={styles.container}>
       <OnboardingSlideIndicator position={currentPosition} />
-      <OnboardingSlider position={currentPosition} onSwipe={(p) => setCurrentPosition(p)} />
+      <OnboardingSlider ref={sliderRef} onSwipe={(p) => setCurrentPosition(p)} />
+
+      <MDButton
+        style={{ marginHorizontal: 16 }}
+        fullWidth={false}
+        label={currentPosition === SLIDE_CNT - 1 ? '완료' : '다음'}
+        onPress={handleNextButtonPress}
+      />
     </MDPage>
   );
 }
@@ -23,8 +48,5 @@ const PageStyles = StyleSheet.create({
   container: {
     paddingTop: 40,
     paddingBottom: 60,
-  },
-  buttonContent: {
-    paddingHorizontal: 16,
   },
 });
--- src/pages/onboarding/ui/OnboardingSlider.tsx
+++ src/pages/onboarding/ui/OnboardingSlider.tsx
@@ -1,20 +1,22 @@
 import { MDColorsType, useThemeColor } from '@shared/lib/theme';
+import { RefObject } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PagerView from 'react-native-pager-view';
 
 interface Props {
-  position: number;
-  onSwipe: (position: number) => void;
+  ref?: RefObject<PagerView | null>;
+  onSwipe?: (position: number) => void;
 }
 
-export function OnboardingSlider({ onSwipe }: Props) {
+export function OnboardingSlider({ ref, onSwipe }: Props) {
   const colors = useThemeColor();
   const styles = SliderStyles({ colors });
 
   return (
     <PagerView
+      ref={ref}
       style={styles.container}
-      onPageSelected={({ nativeEvent }) => onSwipe(nativeEvent.position)}>
+      onPageSelected={({ nativeEvent }) => onSwipe?.(nativeEvent.position)}>
       <View key="1" style={{ backgroundColor: 'red' }}>
         <Text>1</Text>
       </View>

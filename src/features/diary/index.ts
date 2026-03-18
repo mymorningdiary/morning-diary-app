1f87848b8b209447cb03d51ddca6a9a7bdae558c
minuxx <minux8905@gmail.com>
2026-03-18 23:51:06 +0900

    ✨ feat(diary): DiaryAssistant 컴포넌트
    MD-298

--- src/features/diary/index.ts
+++ src/features/diary/index.ts
@@ -1,5 +1,6 @@
-export { WRITING_PLACEHOLDERS } from './lib/writing-messages';
+export { DiaryAssistant } from './ui/DiaryAssistant';
 export { DiaryEditor } from './ui/DiaryEditor';
 export { useDiaryEditor } from './model/useDiaryEditor';
+export { WRITING_PLACEHOLDERS } from './lib/writing-messages';
 export { INACTIVE_TEXT_LEN } from './config/constants';
 export type { DiaryState } from './model/types';

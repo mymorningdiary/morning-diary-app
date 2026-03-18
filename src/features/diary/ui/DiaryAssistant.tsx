feb21d10b22415cba74d2f81ffb508ae2eb413ad
minuxx <minux8905@gmail.com>
2026-03-18 23:51:06 +0900

    ✨ feat(diary): DiaryAssistant 컴포넌트
    MD-298

---
+++ src/features/diary/ui/DiaryAssistant.tsx
@@ -0,0 +1,60 @@
+import { StyleSheet, View } from 'react-native';
+import { Image } from 'expo-image';
+
+import { MDColorsType, useThemeColor } from '@shared/lib/theme';
+import { MDText } from '@shared/ui/Text';
+import { ImgSunBasic } from '@assets/images';
+
+interface Props {
+  image?: string;
+  message: string;
+}
+
+export function DiaryAssistant({ image = ImgSunBasic, message }: Props) {
+  const colors = useThemeColor();
+  const styles = AssistantStyles({ colors });
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <MDText
+        style={styles.message}
+        type="labelSemiBold"
+        color={colors.text.brand}
+        numberOfLines={2}
+        ellipsizeMode="tail">
+        {message}
+      </MDText>
+    </View>
+  );
+}
+
+const AssistantStyles = ({ colors }: { colors: MDColorsType }) =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      backgroundColor: colors.fill.normal,
+      borderRadius: 16,
+      alignItems: 'center',
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      gap: 8,
+      // iOS shadow
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.15,
+      shadowRadius: 3,
+      // Android shadow
+      elevation: 3,
+    },
+    image: {
+      width: 48,
+      height: 48,
+    },
+    message: {
+      flexShrink: 1,
+    },
+  });

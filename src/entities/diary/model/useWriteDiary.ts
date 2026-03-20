9c9da65558f663d9358bee495292806ec088f37b
minuxx <minux8905@gmail.com>
2026-03-20 22:54:13 +0900

    ✨ feat(diary): 일기 작성 API 연동 -> useWriteDiary 훅 추카
    MD-299

---
+++ src/entities/diary/model/useWriteDiary.ts
@@ -0,0 +1,58 @@
+import { useMutation } from '@tanstack/react-query';
+import { postDiaries } from '../api/post-diary';
+import { Logger } from '@shared/lib/log';
+
+interface Options {
+  onSuccess?: ({
+    isFirstWritten,
+    writtenDiaryTextLen,
+  }: {
+    isFirstWritten: boolean;
+    writtenDiaryTextLen: number;
+  }) => void;
+  onError?: (message: string) => void;
+}
+
+export function useWriteDiary({ onSuccess, onError }: Options) {
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: postDiaries,
+    onSuccess: (res) => {
+      if (res.code === 2000) {
+        const { isFirstWrittenDiary, textLength } = res.data;
+        onSuccess?.({ isFirstWritten: isFirstWrittenDiary, writtenDiaryTextLen: textLength });
+      }
+    },
+    onError: (error: any) => {
+      Logger('useWriteDiary').error('Failed to write diary', error);
+      switch (error.code) {
+        case 4200: {
+          onError?.('일기 내용을 적어주세요');
+          break;
+        }
+        case 4203: {
+          onError?.('해당 날짜에 이미 일기를 작성했어요');
+          break;
+        }
+        case 4204:
+        case 4205: {
+          onError?.('일기 작성에 실패했어요 잠시 후 다시 시도해주세요');
+          break;
+        }
+        case 5000:
+        case 5002: {
+          onError?.('서버 오류가 발생했어요 잠시 후 다시 시도해주세요');
+          break;
+        }
+        default: {
+          onError?.('일기 작성에 실패했어요 잠시 후 다시 시도해주세요');
+          break;
+        }
+      }
+    },
+  });
+
+  return {
+    writeDiary: mutateAsync,
+    isPending,
+  };
+}

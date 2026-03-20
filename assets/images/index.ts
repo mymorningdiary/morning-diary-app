94d460efd91f39023da1ce7221ac450713aa1ea6
minuxx <minux8905@gmail.com>
2026-03-21 01:26:30 +0900

    ✨ feat(diary): FirstDiarySlide1 컴포넌트 추가
    MD-300

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -5,5 +5,14 @@ import ImgOnboarding2 from './img-onboarding-2.png';
 import ImgSunBasic from './img-sun-basic.png';
 import ImgSunTwitch from './img-sun-twitch.png';
 import ImgSunSmall from './img-sun-small.png';
+import ImgSunCongrats from './img-sun-congrats.png';
 
-export { ImgTextLogo, ImgOnboarding1, ImgOnboarding2, ImgSunBasic, ImgSunTwitch, ImgSunSmall };
+export {
+  ImgTextLogo,
+  ImgOnboarding1,
+  ImgOnboarding2,
+  ImgSunBasic,
+  ImgSunTwitch,
+  ImgSunSmall,
+  ImgSunCongrats,
+};
--- src/pages/diary-first/ui/FirstDiaryPage.tsx
+++ src/pages/diary-first/ui/FirstDiaryPage.tsx
@@ -1,21 +1,48 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { MDAppBar } from '@shared/ui/AppBar';
 import { MDPage } from '@shared/ui/Layout';
 import { router } from 'expo-router';
 import { DotIndicator } from '@shared/ui/Pagination/DotIndicator';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { FirstDiarySlide1 } from './FirstDiarySlide1';
+import PagerView from 'react-native-pager-view';
+import { MDButton } from '@shared/ui/Button';
 
 export function FirstDiaryPage() {
   const styles = PageStyles;
+  const sliderRef = useRef<PagerView>(null);
 
-  const slides = [{}, {}];
+  const slides = [
+    { key: 'slide1', buttonLabel: '나만의 일기 스타일 고르기', component: <FirstDiarySlide1 /> },
+  ];
   const [currentPosition, setCurrentPosition] = useState(0);
 
   return (
     <MDPage style={styles.container}>
       <MDAppBar onBack={() => router.back()} />
       <DotIndicator style={styles.dotIndicator} position={currentPosition} count={slides.length} />
+
+      <PagerView
+        ref={sliderRef}
+        style={{ flex: 1 }}
+        onPageSelected={({ nativeEvent }) => setCurrentPosition(nativeEvent.position)}>
+        {slides.map((slide) => (
+          <View key={slide.key} style={{ flex: 1 }}>
+            {slide.component}
+          </View>
+        ))}
+      </PagerView>
+
+      <MDButton
+        style={{ marginHorizontal: 16 }}
+        label={slides[currentPosition].buttonLabel}
+        onPress={() => {
+          if (currentPosition < slides.length - 1) {
+            sliderRef.current?.setPage(currentPosition + 1);
+          }
+        }}
+      />
     </MDPage>
   );
 }

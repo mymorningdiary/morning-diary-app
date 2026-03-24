1de42b4f03e8737f843932135aa4cdb69a7c4ed2
minuxx <minux8905@gmail.com>
2026-03-24 22:16:25 +0900

    ✨ feat(account): 로그아웃 API 연동 -> useSignOut 훅 추가
    MD-305

--- src/pages/account/ui/AccountManagementSection.tsx
+++ src/pages/account/ui/AccountManagementSection.tsx
@@ -1,24 +1,41 @@
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
 
-import { MDColorsType, useThemeColor } from '@shared/lib/theme';
+import { useSignOut } from '@entities/auth';
+import { useToastStore } from '@shared/lib/toast';
 import { MDListItem, MDSection } from '@shared/ui/Layout';
+import { MDModal } from '@shared/ui/Modal';
 
 interface Props {
   isLast?: boolean;
 }
 
 export function AccountManagementSection({ isLast }: Props) {
-  const colors = useThemeColor();
-  const styles = Styles({ colors });
+  const { signOut, isPending } = useSignOut({
+    onError: (message) => {
+      if (!message) return;
+      useToastStore.getState().show({ type: 'error', message });
+    },
+  });
+
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
+
+  const handleLogout = () => {
+    setShowLogoutModal(false);
+    if (isPending) return;
+    signOut();
+  };
 
   return (
     <MDSection isLast={isLast}>
-      <MDListItem label="로그아웃" />
+      <MDListItem label="로그아웃" onPress={() => setShowLogoutModal(true)} />
+
+      <MDModal
+        visible={showLogoutModal}
+        subtitle={`로그아웃 하시겠어요?`}
+        negative={{ text: '취소', onPress: () => setShowLogoutModal(false) }}
+        positive={{ text: '로그아웃', onPress: handleLogout }}
+        onClose={() => setShowLogoutModal(false)}
+      />
     </MDSection>
   );
 }
-
-const Styles = ({ colors }: { colors: MDColorsType }) =>
-  StyleSheet.create({
-    container: {},
-  });

ca64bfdc280bbe9bb273eb91578b7d938ebd7ef6
minuxx <minux8905@gmail.com>
2026-03-25 23:43:16 +0900

    ✨ feat(notification): 알림 시간 설정 로직 -> useNotificationTime 훅 추가
    MD-306

---
+++ src/features/notification/model/types.ts
@@ -0,0 +1,4 @@
+export interface NotificationTime {
+  hours: number;
+  minutes: number;
+}
---
+++ src/features/notification/model/useNotificationTime.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import { NotificationTime } from './types';
+import { useUser } from '@entities/user';
+
+export function useNotificationTime() {
+  const { user } = useUser();
+  const [currentTime, setCurrentTime] = useState<NotificationTime>({ hours: 7, minutes: 0 });
+
+  const formattedTime = dayjs()
+    .hour(currentTime.hours)
+    .minute(currentTime.minutes)
+    .locale('en')
+    .format('hh:mm A');
+
+  useEffect(() => {
+    if (!user || !user.alarmTime) return;
+
+    const [hours, minutes] = user.alarmTime.split(':').map(Number);
+    setCurrentTime({ hours, minutes });
+  }, [user]);
+
+  return {
+    currentTime,
+    formattedTime,
+    setCurrentTime,
+  };
+}

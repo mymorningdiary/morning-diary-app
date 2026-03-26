81b7553b665f1d37b1b1a756bde553279e9f1ea1
minuxx <minux8905@gmail.com>
2026-03-26 22:43:36 +0900

    ♻️ refactor: 불필요한 파일 정리

--- core/api/index.ts
+++ core/api/index.ts
@@ -1,6 +1,4 @@
-import authAPI from './auth/apis';
 import diaryAPI from './diary/apis';
-import userAPI from './user/apis';
 
 export type * from './types';
-export { authAPI, diaryAPI, userAPI };
+export { diaryAPI };

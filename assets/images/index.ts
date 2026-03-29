55a6e40b8dad0f92f64f625ad81782d35ecc761d
minuxx <minux8905@gmail.com>
2026-03-29 23:01:55 +0900

    ✨ feat(home): +DiaryCalendarDay 컴포넌트 추가
    MD-307

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -7,6 +7,12 @@ import ImgSunTwitch from './img-sun-twitch.png';
 import ImgSunSmall from './img-sun-small.png';
 import ImgSunCongrats from './img-sun-congrats.png';
 
+import ImgEmotion1 from './img-emotion-1.png';
+import ImgEmotion2 from './img-emotion-2.png';
+import ImgEmotion3 from './img-emotion-3.png';
+import ImgEmotion4 from './img-emotion-4.png';
+import ImgEmotion5 from './img-emotion-5.png';
+
 export {
   ImgTextLogo,
   ImgOnboarding1,
@@ -15,4 +21,9 @@ export {
   ImgSunTwitch,
   ImgSunSmall,
   ImgSunCongrats,
+  ImgEmotion1,
+  ImgEmotion2,
+  ImgEmotion3,
+  ImgEmotion4,
+  ImgEmotion5,
 };
---
+++ src/pages/home/ui/Calendar/DiaryCalendarDay.tsx
@@ -0,0 +1,70 @@
+import { StyleSheet, View } from 'react-native';
+import { Image } from 'expo-image';
+import dayjs from 'dayjs';
+
+import { MDColorsType, useThemeColor } from '@shared/lib/theme';
+import { MDText } from '@shared/ui/Text';
+import { ImgEmotion1, ImgEmotion2, ImgEmotion3, ImgEmotion4, ImgEmotion5 } from '@assets/images';
+import { DayState } from 'react-native-calendars/src/types';
+
+const selectEmotionImage = (emotion: number) => {
+  if (emotion <= 20) {
+    return ImgEmotion1;
+  }
+  if (emotion <= 40) {
+    return ImgEmotion2;
+  }
+  if (emotion <= 60) {
+    return ImgEmotion3;
+  }
+  if (emotion <= 80) {
+    return ImgEmotion4;
+  }
+  if (emotion <= 100) {
+    return ImgEmotion5;
+  }
+};
+
+interface Props {
+  date?: string;
+  state?: DayState;
+  emotion?: number | null;
+  onDateSelect?: (date: string) => void;
+}
+
+export function DiaryCalendarDay({ date, state, emotion = null, onDateSelect }: Props) {
+  const colors = useThemeColor();
+  const styles = DayStyles({ colors });
+
+  const formattedDate = dayjs(date).format('D');
+
+  return (
+    <View style={styles.container}>
+      {emotion != null && (
+        <Image style={styles.emotionImage} source={selectEmotionImage(emotion)} />
+      )}
+      {emotion == null && (
+        <MDText
+          type="numberRegular"
+          color={state === 'disabled' ? colors.text.alternative : colors.text.brand}>
+          {formattedDate}
+        </MDText>
+      )}
+    </View>
+  );
+}
+
+const DayStyles = ({ colors }: { colors: MDColorsType }) =>
+  StyleSheet.create({
+    container: {
+      width: 48,
+      height: 48,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: colors.background.normal,
+    },
+    emotionImage: {
+      width: 40,
+      height: 40,
+    },
+  });

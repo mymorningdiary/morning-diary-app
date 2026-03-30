8875ce599b839c4e544cd3dfe011be7a67fcc60b
minuxx <minux8905@gmail.com>
2026-03-30 14:33:02 +0900

    ✨ feat(diary-list): DiaryListPage 초기화
    MD-286

--- components/MDCol.tsx
+++
@@ -1,9 +0,0 @@
-import { View, type ViewProps } from 'react-native';
-
-type MDColProps = ViewProps;
-
-export function MDCol({ style, ...rest }: MDColProps) {
-  return (
-    <View style={[{ backgroundColor: 'transparent', flexDirection: 'column' }, style]} {...rest} />
-  );
-}
--- components/MDRow.tsx
+++
@@ -1,9 +0,0 @@
-import { View, type ViewProps } from 'react-native';
-
-type MDRowProps = ViewProps;
-
-export function MDRow({ style, ...rest }: MDRowProps) {
-  return (
-    <View style={[{ backgroundColor: 'transparent', flexDirection: 'row' }, style]} {...rest} />
-  );
-}

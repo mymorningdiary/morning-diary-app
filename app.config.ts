da6441e0aa53cdbcd11f847eb5425581ac02d50c
minuxx <minux8905@gmail.com>
2026-03-30 14:35:35 +0900

    ♻️ refactor: 불필요 이미지 에셋 정리
    MD-307

--- app.config.ts
+++ app.config.ts
@@ -61,7 +61,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   web: {
     bundler: 'metro',
     output: 'static',
-    favicon: './assets/images/favicon.png',
+    favicon: './assets/images/img-sun-small.png',
   },
   plugins: [
     'expo-router',

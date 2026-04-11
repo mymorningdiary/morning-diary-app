d53f689901735726da8ebf0e782ca3d2ab488df5
minuxx <minux8905@gmail.com>
2026-04-11 16:46:15 +0900

    ♻️ refactor: DatePicker 내부에 DatePickerModal 배치
    MD-313

--- src/pages/diary-list/ui/DiaryListPage.tsx
+++ src/pages/diary-list/ui/DiaryListPage.tsx
@@ -1,15 +1,19 @@
 import { StyleSheet } from 'react-native';
 
-import { MDPage } from '@shared/ui/Layout';
-import { MDText } from '@shared/ui/Text';
 import { DiaryPreviewCard } from '@features/diary';
+import { MDPage } from '@shared/ui/Layout';
+import { DatePicker } from '@shared/ui/Picker';
+import dayjs from 'dayjs';
+import { useState } from 'react';
 
 export function DiaryListPage() {
   const styles = PageStyles;
 
+  const [currentDate, setCurrentDate] = useState(dayjs().format('YYYY-MM-DD'));
+
   return (
     <MDPage style={styles.container}>
-      <MDText>DiaryList Page</MDText>
+      <DatePicker date={currentDate} onDateChange={setCurrentDate} />
       <DiaryPreviewCard
         emotion={5}
         title="대충 긴 텍스트를 적어보자 대충 긴 텍스트를 적어보자 대충 긴 텍스트를 적어보자"

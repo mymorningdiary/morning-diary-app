e4160e374364834aa59001ec96f4deb3a9fa44b1
minuxx <minux8905@gmail.com>
2026-05-13 21:06:57 +0900

    🔧 chore(theme): +온글잎 레디오 볼펜체 설정
    MD-320

--- src/shared/lib/theme/types/fonts.ts
+++ src/shared/lib/theme/types/fonts.ts
@@ -1,8 +1,8 @@
 import { MDFonts } from '../const/typography';
 
 export type MDFontFamily = {
-  fontFamily: 'Pretendard' | 'Roboto' | 'Inter';
-  fontWeight: 700 | 600 | 500 | 400 | 400 | 400;
+  fontFamily: 'Pretendard' | 'Roboto' | 'Inter' | 'OngleLeafRadioPen';
+  fontWeight: 700 | 600 | 500 | 400;
 };
 
 export type MDFontsType = keyof typeof MDFonts;

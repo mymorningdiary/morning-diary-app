5d5129e56c42b5b677129040b59a8edb6ecb6ccc
minuxx <minux8905@gmail.com>
2026-05-29 01:20:57 +0900

    💄 style(pages/diary-list): DiaryWeeklyReportItem 보더 컬러 변경

--- src/pages/diary-list/ui/DiaryListWeeklyReportItem.tsx
+++ src/pages/diary-list/ui/DiaryListWeeklyReportItem.tsx
@@ -1,7 +1,6 @@
 import { IconWeeklyReport } from '@assets/icons';
 import { useThemeColor } from '@shared/lib/theme';
 import { MDText } from '@shared/ui/Text';
-import { LinearGradient } from 'expo-linear-gradient';
 import { router } from 'expo-router';
 import { Pressable, StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 
@@ -16,35 +15,32 @@ export function DiaryWeeklyReportItem({ style, title, reportId }: Props) {
 
   return (
     <View style={{ paddingHorizontal: 16 }}>
-      <LinearGradient
-        colors={['#FF8733', '#FFBA8A']}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-        style={[styles.gradientBorder, style]}>
-        <Pressable
-          style={[styles.container, { backgroundColor: colors.fill.normal }]}
-          onPress={() => router.push(`/report/${reportId}`)}>
-          <IconWeeklyReport width={32} height={32} />
+      <Pressable
+        style={[
+          styles.container,
+          {
+            backgroundColor: colors.fill.normal,
+            borderWidth: 1,
+            borderColor: colors.primary.light,
+          },
+        ]}
+        onPress={() => router.push(`/report/${reportId}`)}>
+        <IconWeeklyReport width={32} height={32} />
 
-          <View style={styles.textContent}>
-            <MDText type="caption1Regular" color={colors.text.alternative}>
-              주간리포트
-            </MDText>
-            <MDText type="labelSemiBold" color={colors.text.brand} numberOfLines={2}>
-              {title}
-            </MDText>
-          </View>
-        </Pressable>
-      </LinearGradient>
+        <View style={styles.textContent}>
+          <MDText type="caption1Regular" color={colors.text.alternative}>
+            주간리포트
+          </MDText>
+          <MDText type="labelSemiBold" color={colors.text.brand} numberOfLines={2}>
+            {title}
+          </MDText>
+        </View>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  gradientBorder: {
-    borderRadius: 16,
-    padding: 1,
-  },
   container: {
     flexDirection: 'row',
     alignItems: 'center',
